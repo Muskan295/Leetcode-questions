fcc631b591b7fb9563a1d144d4032146cc8cf8c6
Muskan <muskanmittal249@gmail.com>
2025-10-29 12:47:30 +0530

    Remove elements from linked list

--- removeElement.java
+++ removeElement.java
@@ -1,12 +1,12 @@
-public class ListNode {
+class ListNode {
      int val;
      ListNode next;
      ListNode() {}
      ListNode(int val) { this.val = val; }
      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
-class Solution {
-    public ListNode removeElements(ListNode head, int val) {
+public class removeElement {
+    ListNode removeElements(ListNode head, int val) {
         if(head==null){
             return head;
         }
