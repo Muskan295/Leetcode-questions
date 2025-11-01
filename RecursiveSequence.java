7e6ad7a320a39e1caea51012151e5138301c60c7
Muskan <muskanmittal249@gmail.com>
2025-11-01 10:32:10 +0530

    Recursive sequence Solution

---
+++ RecursiveSequence.java
@@ -0,0 +1,19 @@
+public class RecursiveSequence {
+    static long sequence(int n) {
+        long sum=0;
+        long start=1;
+        final long  MOD=1000000007;
+        
+        
+        for(int i=1;i<=n;i++){
+            long product=1;
+            for(long j=start;j<start+i;j++){
+                product=(product*j) % MOD;
+            }
+                sum=(sum+product)%MOD;
+                start=start+i;
+                
+        }
+        return sum;
+    }
+}
